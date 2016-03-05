cb74f3828e35ad3f807868af8e672da1947416a4
Manoj Desaraju <manoj.desaraju@gmail.com>
2016-03-04 21:20:34 -0600

    HW3 part3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,13 +12,29 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings=Movie.all_ratings
-    @rating_arr=@all_ratings
-    if params[:ratings]!=nil
-    	@rating_arr=params[:ratings].keys
+    # convert all_ratings arr into a hash
+    if(@all_ratings_map==nil)
+      @all_ratings_map = Hash.new
+      @all_ratings.each do |rating|
+        @all_ratings_map[rating]=1
+      end
     end
+    # set the session variable ratings only when we dont uncheck all the ratings
+    # if this method is called for the first time, set the sessions variable
+    if params[:ratings]!=nil
+	session[:ratings]=params[:ratings]
+    elsif session[:ratings]==nil
+	session[:ratings]=@all_ratings_map
+    end
+    # if uri request has a value for sort, store it in session.
     if params[:sort].present?
-      @movies = Movie.order(params[:sort])
-      if params[:sort]=='title'
+      session[:sort]=params[:sort]
+    end
+    # if session has stored a value for sort, then order the movies.
+    if session[:sort].present?
+      @movies = Movie.where(:rating=>session[:ratings].keys).order(session[:sort])
+      # set the corresponding variable to hilite =>used in the view to color a heading yellow
+      if session[:sort]=='title'
         params[:title_header]= 'hilite'
         params[:release_date_header]= nil
       else
@@ -26,7 +42,7 @@ class MoviesController < ApplicationController
         params[:release_date_header]= 'hilite'
       end
     else
-      @movies = Movie.where(:rating=>@rating_arr)
+      @movies = Movie.where(:rating=> session[:ratings].keys)
     end
   end
 
@@ -37,7 +53,9 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    flash.keep
+    # for the uri to show the chosen ratings and sort
+    redirect_to movies_path(:sort=>session[:sort],:ratings=>session[:ratings])
   end
 
   def edit
@@ -48,6 +66,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
+    flash.keep
     redirect_to movie_path(@movie)
   end
 
@@ -55,7 +74,10 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
+    flash.keep
+    #redirect_to movies_path
+    # for the uri to show the chosen ratings and sort
+    redirect_to movies_path(:sort=>session[:sort],:ratings=>session[:ratings])
   end
 
 end
